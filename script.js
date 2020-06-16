ab07fbffc26105d93045a10b8e00832fbc91aa68
Alexandr <43585699+carachevtsev@users.noreply.github.com>
2020-06-16 23:54:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -118,6 +118,22 @@ function startWebRTC(isOfferer) {
 
 function startListentingToSignals() {
   // Listen to signaling data from Scaledrone
+  pc.ontrack = event => {
+    const stream = event.streams[0];
+    if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
+      remoteVideo.srcObject = stream;
+    }
+  };
+
+  navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  }).then(stream => {
+    // Display your local video in #localVideo element
+    localVideo.srcObject = stream;
+    // Add your stream to be sent to the conneting peer
+    stream.getTracks().forEach(track => pc.addTrack(track, stream));
+  }, onError);
   room.on('data', (message, client) => {
     // Message was sent by us
     if (client.id === drone.clientId) {
