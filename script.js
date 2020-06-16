70e2e8e0208b9e52afafe848fc7a8f5d176c54d4
Alexandr <43585699+carachevtsev@users.noreply.github.com>
2020-06-17 00:27:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -95,6 +95,24 @@ function startWebRTC(isOfferer) {
     }
   }
 
+   // When a remote stream arrives display it in the #remoteVideo element
+  pc.ontrack = event => {
+    const stream = event.streams[0];
+    if (!remoteVideo.srcObject || remoteVideo.srcObject.id !== stream.id) {
+      remoteVideo.srcObject = stream;
+    }
+  };
+
+  navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  }).then(stream => {
+    // Display your local video in #localVideo element
+    localVideo.srcObject = stream;
+    // Add your stream to be sent to the conneting peer
+    stream.getTracks().forEach(track => pc.addTrack(track, stream));
+  }, onError);
+  
   room.on('data', (message, client) => {
     // Message was sent by us
     if (client.id === drone.clientId) {
