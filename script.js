472264c1b147f7cf14ead14b4d273f27a75fb67e
Alexandr <43585699+carachevtsev@users.noreply.github.com>
2020-06-22 20:58:26 +0300

    Update script.js

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const drone = new ScaleDrone('b4hwSfoKT8CniGIh');
 const roomName = 'observable-' + roomHash;
 const configuration = {
   iceServers: [{
-    urls: 'stun:stun.sipnet.ru:3478'
+    urls: 'stun:stun1.l.google.com:19302'
   }]
 };
 let room;
